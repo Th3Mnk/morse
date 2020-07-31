6f47d9d4aa1191f8e391c31857dbe8741e5d6c21
Th3Mnk <thelonius.lucien@sascholar.org>
2020-07-31 14:48:57 +0000

    backup

---
+++ app.py
@@ -0,0 +1,25 @@
+# ---- YOUR APP STARTS HERE ----
+# -- Import section --
+from flask import Flask
+from flask import render_template
+from flask import request
+from model import morse
+
+
+# -- Initialization section --
+app = Flask(__name__)
+
+
+# -- Routes section --
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/results', methods=['GET', 'POST'])
+def results():
+    morseMsg = request.form["Morse"]
+    directions = request.form["input"]
+    pants = morse(morseMsg, directions)
+    return render_template("results.html", directions=directions, morseMsg=morseMsg, pants=pants)
